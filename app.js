c7980ccfb03d65a52d1bd44a96df1ca5436fc46d
Samuel McTaggart <sam.mctaggart@gmail.com>
2014-05-31 15:30:41 +0100

    change express.static

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(require('node-compass')({mode: 'expanded'}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(expressLayouts);
 
 app.use('/', routes);
